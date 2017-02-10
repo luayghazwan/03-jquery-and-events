f8f152850ac3fb01225a5f640a6c434e9dead9b0
‘LuayYounus’ <luayghazwan@yahoo.com>
2017-02-10 06:48:06 -0800

    navBar handler to hide tabs when clicking on it

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -55,11 +55,13 @@ articleView.handleCategoryFilter = function() {
 
 articleView.handleMainNav = function () {
   $('.main-nav').on('click', '.tab', function() {
-    /* TODO:
+    /* DONE:
       1. Hide all of the .tab-content sections
       2. Fade in the single .tab-content section that is
         associated with the .tab element's data-content attribute.
     */
+    $('.tab-content').hide();
+    $('#' + $(this).data('content')).fadeIn();
   });
   $('.main-nav .tab:first').click();
 };
