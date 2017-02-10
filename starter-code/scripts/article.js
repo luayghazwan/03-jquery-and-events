54b6a0fa3e7dead633e4a3242c3630b967f81d09
‘LuayYounus’ <luayghazwan@yahoo.com>
2017-02-10 05:42:34 -0800

    Added Author, category data-attributes to article.js

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -12,9 +12,10 @@ function Article (opts) {
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone().removeClass('template');
 
-  $newArticle.attr('data-category', this.category);
   // DONE: Use jQuery to also add the author name as a data-attribute of the newly cloned article.
   //       Doing so will allow us to use selectors to target articles, based on who wrote them.
+  $newArticle.attr('data-author', this.author);
+  $newArticle.attr('data-category', this.category);
 
   $newArticle.find('.byline a').text(this.author);
   $newArticle.find('.byline a').attr('href', this.authorUrl);
